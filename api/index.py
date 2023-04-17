8910c66abd8b6da0ab1652132438810137be6cd2
C <iamCristYe@Gmail.com>
2023-04-17 13:24:55 +0800

    test

--- api/index.py
+++ api/index.py
@@ -1,7 +1,15 @@
 from flask import Flask
 import os
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return os.environ.get("telegram_key")
+app = Flask(__name__)
+bot = telebot.TeleBot(os.environ.get("telegram_key"))
+
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(
+        url="https://bot-telegram-dates-reminder.vercel.app/"
+        + os.environ.get("telegram_key")
+    )
+    return 'Chat with the Bot  <a href ="https://t.me/DatesReminderBot">here</a> or   Check the project code <a href ="https://github.com/mdipietro09/Bot_TelegramDatesReminder">here</a>'
