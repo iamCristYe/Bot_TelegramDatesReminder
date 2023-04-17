d787c653cd1d56e16f2c439864334a3625c5428d
C <iamCristYe@Gmail.com>
2023-04-17 13:06:48 +0800

    test

--- api/index.py
+++ api/index.py
@@ -4,22 +4,22 @@ from flask import Flask
 
 
 
-bot = telebot.TeleBot(os.environ.get("telegram_key"))
+# bot = telebot.TeleBot(os.environ.get("telegram_key"))
 
-# /start
-@bot.message_handler(commands=['start'])
-def _start(message):
-    ## send first msg
-    msg = "Hello "+str(message.chat.username)+\
-          ", I'm a date reminder. Tell me birthdays and events to remind you. To learn how to use me, use \n/help"
-    bot.send_message(message.chat.id, msg)
+# # /start
+# @bot.message_handler(commands=['start'])
+# def _start(message):
+#     ## send first msg
+#     msg = "Hello "+str(message.chat.username)+\
+#           ", I'm a date reminder. Tell me birthdays and events to remind you. To learn how to use me, use \n/help"
+#     bot.send_message(message.chat.id, msg)
 
 app = flask.Flask(__name__)
 
-@app.route('/'+os.environ.get("telegram_key"), methods=['POST'])
-def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
-        return "!", 200
+# @app.route('/'+os.environ.get("telegram_key"), methods=['POST'])
+# def getMessage():
+#         bot.process_new_updates([telebot.types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
+#         return "!", 200
 
 @app.route("/")
 def webhook():
